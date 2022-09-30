3ac0ce0476d1375412b68ec6f2bdaeb08a8b043b
TemesgeA <temesgen.ayneka@gmail.com>
2022-09-30 23:55:24 +0300

    Add here section

---
+++ src/components/hero.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import heroBanner from '../images/Group 77.png';
+
+function Hero() {
+    return (
+        <div className="hero">
+            <img src={heroBanner} alt="Heros banner" className='hero-image' />
+            <h1 className='hero-title'>Online Experiences</h1>
+            <p className='hero-content'>Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.
+</p>
+        </div>
+    );
+}
+
+export default Hero;
