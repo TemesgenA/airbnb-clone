29fd72dc409951c1f2c971c24f27b4153cdb8b88
TemesgeA <temesgen.ayneka@gmail.com>
2022-10-01 00:15:26 +0300

    modify style and its className

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -5,7 +5,7 @@ function Hero() {
     return (
         <div className="hero">
             <img src={heroBanner} alt="Heros banner" className='hero-image' />
-            <h1 className='hero-title'>Online Experiences</h1>
+            <h1 className='hero-heading'>Online Experiences</h1>
             <p className='hero-content'>Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.
 </p>
         </div>
