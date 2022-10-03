e18673bfcba7bbffeb55a3770719f06c09ef1cb7
TemesgeA <temesgen.ayneka@gmail.com>
2022-10-03 17:55:14 +0300

    import Card component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import Nav from './components/navbar';
 import Hero from './components/hero';
+import Card from './components/card';
 import './App.css';
+import image from './images/katie-zaferes.png';
 
 function App() {
   return (
     <div className="App">
       <Nav />
       <Hero />
+      <Card 
+      img= {image}
+      rating= '5.0'
+      reviewCount= '6'
+      country= 'USA'
+      title= 'Life Lessons with Katie Zaferes'
+      price= '136'
+      />
     </div>
   );
 }
